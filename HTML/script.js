cb80b0dd0f21e2cd8a08865f5a50769f3fd1b82e
dmorgan <dmorgan@5b7c3c08-40e5-403b-9995-ace06908e4af>
2011-02-07 23:18:17 +0000

    Import cleaned indexhtml
    git-svn-id: svn://svn.mageia.org/svn/soft/indexhtml/trunk@485 5b7c3c08-40e5-403b-9995-ace06908e4af

---
+++ HTML/script.js
@@ -0,0 +1,95 @@
+/**
+ * @copyright Copyright (c) 2011 Romain d'Alverny
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GNU GPL v3 and later
+ *
+ * Redirect browser online IF it is in online mode.
+ * Or stay on offline page and listen to online event.
+ *
+ * Browser targets: Mozilla Firefox 3.6+, Konqueror.
+*/
+
+function StartPage(url, image) {
+    this.location = url;
+    this.onlinePx = image;
+}
+
+StartPage.prototype.run = function () {
+    if (false && navigator.onLine !== null) {
+        !navigator.onLine ? this.actOnline() : this.actOffline();
+
+    } else { // for khtml-based browsers
+        try {
+            var sp = this,
+                i = document.createElement('img');
+
+            window.alert = function() {};
+
+            i.id = 'i';
+            i.onload = function () { console.log(sp); sp.actOnline(); };
+            i.onerror = function () { sp.actOffline(); };
+            i.src = this.onlinePx + '?' + new Date().getTime();
+            document.getElementsByTagName('body').item(0).appendChild(i);
+        }
+        catch (e) { return false; }
+    }
+
+    return true;
+}
+
+StartPage.prototype.actOnline = function () {
+    var productId = '',
+        lang = '',
+        args = new Array;
+
+    if (null !== (product_id = this.getProductId())) {
+        args.push('p=' + product_id);
+    }
+
+    if (null !== (lang = this.getLocale())) {
+        args.push('l=' + lang);
+    }
+
+    if (args.length > 0) {
+        this.location = [this.location, args.join('&')].join('?');
+    }
+    parent.location = this.location;
+
+    return true;
+}
+
+StartPage.prototype.actOffline = function () {
+    var p = this,
+        i = document.getElementById("i");
+
+    if (null !== i) {
+        i.parentNode.removeChild(i);
+    }
+    document.getElementsByTagName("body").item(0).setAttribute("class", "offline");
+    document.body.addEventListener("online", function () { p.actOnline(); }, false);
+
+    return true;
+}
+
+StartPage.prototype.getLocale = function () {
+    var ret = null;
+
+    try {
+        ret = parent.window.document.documentElement.attributes.getNamedItem('lang').value.trim();
+    } catch (e) {}
+
+    return ret;
+}
+
+StartPage.prototype.getProductId = function () {
+    var ret = null,
+        t = document.getElementsByTagName('meta');
+
+    for (var i = 0; i < t.length; i += 1) {
+        if (t.item(i).getAttribute('name') == 'product:id') {
+            ret = t.item(i).getAttribute('content').trim();
+            break;
+        }
+    }
+
+    return ret;
+}
---
+++ mail/placeholder.h
@@ -0,0 +1,27 @@
+/*
+ * This file is a placeholder; to define strings to be translated for
+ * the welcome mail message.
+ */
+
+char *placeholder[] = {
+	/* name appearing in To: */
+	i18n("Mageia Users"),
+	/* subject */
+	i18n("Welcome to Mageia"),
+	/* */
+	i18n("Welcome to Mageia!"),
+	/* mail greeting heading */
+	i18n("Hello,"),
+	/* */
+	i18n("We hope you are totally satisfied with Mageia. Below is a list of useful websites:"),
+	/* */
+	"Mageia",
+	"http://www.mageia.org/",
+	/* placeholer is mageia.org URL */
+	i18n("The %s website provides all the details for keeping in touch with the community."),
+	/* goodbye signature (1st line) */
+	i18n("Yours Sincerely,"),
+	/* goodbye signature (2nd line) */
+	i18n("The Mageia team"),
+};
+
