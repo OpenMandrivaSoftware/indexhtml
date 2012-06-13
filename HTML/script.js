0215ca38aa20b7244d4f5a449efc405d27d55232
rda <rda@5b7c3c08-40e5-403b-9995-ace06908e4af>
2012-06-13 15:28:25 +0000

    redirect directly, without checking on/offline status
    git-svn-id: svn://svn.mageia.org/svn/soft/indexhtml/trunk@4911 5b7c3c08-40e5-403b-9995-ace06908e4af

--- HTML/script.js
+++ HTML/script.js
@@ -1,48 +1,21 @@
 /**
- * @copyright Copyright (c) 2011 Romain d'Alverny
+ * @copyright Copyright (c) 2011-2012 Romain d'Alverny
  * @license http://www.gnu.org/licenses/gpl-3.0.html GNU GPL v3 and later
  *
- * Redirect browser online IF it is in online mode.
- * Or stay on offline page and listen to online event.
+ * Redirect browser to URL with GET parameters:
+ * - l = html[lang]
+ * - p = meta[product:id]
  *
- * Browser targets: Mozilla Firefox 3.6+, Konqueror.
 */
 
-function StartPage(url, image) {
+function StartPage(url) {
     this.location = url;
-    this.onlinePx = image;
 }
 
 StartPage.prototype.run = function () {
-    if (navigator.onLine !== null) {
-        if (navigator.onLine) {
-          this.actOnline();
-        } else {
-          this.actOffline();
-        }
-    } else { // for khtml-based browsers
-        try {
-            var sp = this,
-                i = document.createElement('img');
-
-            window.alert = function() {};
-
-            i.id = 'i';
-            i.onload = function () { console.log(sp); sp.actOnline(); };
-            i.onerror = function () { sp.actOffline(); };
-            i.src = this.onlinePx + '?' + new Date().getTime();
-            document.getElementsByTagName('body').item(0).appendChild(i);
-        }
-        catch (e) { return false; }
-    }
-
-    return true;
-};
-
-StartPage.prototype.actOnline = function () {
     var product_id = '',
-        lang = '',
-        args = [];
+        lang       = '',
+        args       = [];
 
     document.getElementsByTagName("body").item(0).setAttribute("class", "");
 
@@ -58,19 +31,6 @@ StartPage.prototype.actOnline = function () {
         this.location = [this.location, args.join('&')].join('?');
     }
     parent.location = this.location;
-
-    return true;
-};
-
-StartPage.prototype.actOffline = function () {
-    var p = this,
-        i = document.getElementById("i");
-
-    if (null !== i) {
-        i.parentNode.removeChild(i);
-    }
-    document.getElementsByTagName("body").item(0).setAttribute("class", "offline");
-    document.body.addEventListener("online", function () { p.actOnline(); }, false);
 
     return true;
 };
